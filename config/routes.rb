b6171cfbd1f38b052fcf356d36d00c321a088cbe
takemasa32 <takemasa0302@gmail.com>
2023-11-20 04:09:54 +0900

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,16 @@ Rails.application.routes.draw do
   # root "articles#index"
   root 'home#index'
 
-  get ':id', to: 'home#show', as: 'custom_blog'
+  get 'comtents/:id', to: 'home#show', as: 'custom_blog'
+
+  # 認証機能
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/profile', to: 'users#show'
+  get '/unsubscribe', to: 'users#confirm_destroy'
+  delete '/unsubscribe', to: 'users#destroy'
+  #　ログイン系
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_19_164447) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_19_175723) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -20,6 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_19_164447) do
     t.datetime "updated_at", null: false
     t.string "post_image"
     t.string "image_cache"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
